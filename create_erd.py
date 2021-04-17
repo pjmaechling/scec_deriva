5d6f788d81de51c4886a95bf2620de7ef133e940
Philip Maechling <pjmaechling@users.noreply.github.com>
2021-04-17 11:13:48 -0700

    Update create_erd.py
    Second version of the Chisel create_erd script to support SCEC ETAS forecast data

--- create_erd.py
+++ create_erd.py
@@ -1,64 +1,135 @@
-1 contributor
-63 lines (48 sloc) 2.23 KB
-#
-# Test the deriva-client library
-# Based on a pip install into a anaconda python3 installation
-#
-from deriva.core import DerivaServer, ErmrestCatalog, HatracStore, AttrDict, get_credential
-from deriva.core.ermrest_model import builtin_types, Table, Column, Key, ForeignKey
+"""Minimal example of a possible rendering of the ETAS starter model.
+Developer: Robert Schuler
+Modifications: Philip Maechling
+"""
+from deriva.core import DerivaServer, get_credential
+from deriva.chisel import Model, Schema, Table, Column, Key, ForeignKey, builtin_types, tag
 import sys
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    try:
 
-        scheme = "https"
-        catalog_id = "2"
-        servername = "forecast.derivacloud.org"
+# Connect to server and catalog ------------------------------------------------------------------#
 
-        credentials = get_credential(servername)
-        print(type(credentials))
+hostname = 'forecast.derivacloud.org'   # this is a dev server for throw-away work (change to 'forecast.derivacloud.org)
+catalog_id = '5'            # this was a throw-away catalog used to test this script (change to TBD)
 
-        catalog = ErmrestCatalog(scheme, servername, catalog_id, credentials=credentials)
-        print(type(catalog))
-        print(str(catalog))
+model = Model.from_catalog(
+    DerivaServer('https', hostname, credentials=get_credential(hostname)).connect_ermrest(catalog_id)
+)
 
-        model = catalog.getCatalogModel()
-        client_table = model.schemas["public"].tables["ermrest_client"]
+# Cleanup ----------------------------------------------------------------------------------------#
 
-        # you can access the column definitions in their defined order in the table
-        for column in client_table.column_definitions:
-            print((column.name, column.type.typename, column.nullok))
-
-        # you can also look up columns by name or by position
-        column = client_table.column_definitions[0]
-        assert column == client_table.column_definitions[column.name]
+if 'ETAS' in model.schemas:
+    # Purge anything so we can "do over" repeatedly at this point
+    model.schemas['ETAS'].drop(cascade=True)
 
 
+# ETAS schema ------------------------------------------------------------------------------------#
 
-        # replace these with your real group IDs
-        grps = AttrDict({
-            "admin": "https://app.globus.org/groups/50b309c9-4223-11eb-8a64-0ece49b2bd8d",
-            "curator": "https://app.globus.org/groups/e5e0880b-4222-11eb-8c49-0eb9d45bd5fd",
-            "writer": "https://app.globus.org/groups/a060fd0b-3f13-11eb-85ca-0aecec13621f",
-            "reader": "https://app.globus.org/groups/ebfd0f5d-4221-11eb-8826-0aecec13621f",
-        })
+# Create the "schema" to organize tables in the catalog "model"
+model.create_schema(Schema.define('ETAS'))
 
-        object_store = HatracStore('https', servername, credentials)
-        namespace = '/hatrac/project_data'
-        object_store.create_namespace(namespace)
-        object_store.set_acl(namespace, 'owner', [grps.admin])
-        object_store.set_acl(namespace, 'subtree-create', [grps.curator, grps.writer])
-        object_store.set_acl(namespace, 'subtree-update', [grps.curator])
-        object_store.set_acl(namespace, 'subtree-read', [grps.reader])
+# ETAS Forecast ----------------------------------------------------------------------------------#
+
+# Create the tables at the "Forecast" level of the table hierarchy
+
+# ETAS_Forecast
+model.schemas['ETAS'].create_table(Table.define(  # <--- 'Table.define(...)' defines a general-purpose table
+    'ETAS_Forecast',                                                        # table name
+    column_defs=[                                                           # column definitions
+        Column.define('Forecast_Name', builtin_types.text, nullok=False),   # column for the name of the forecast
+    ],
+    key_defs=[                                                              # key definitions
+        Key.define(['Forecast_Name'])                                       # each key specifies its list of columns of 1+ columns
+    ]
+))
 
 
-    except Exception as e:
-        print(e)
-        print("Caught Exception")
-    else:
-        print("Success!")
+# ETAS_Forecast_File
+#  This is for storing the 'forecast_config.json', 'slurm_log', 'slurm_stdout', and 'slurm_stderr'
+model.schemas['ETAS'].create_table(Table.define_asset(  # <--- 'Table.define_asset(...)' defines a table for storing files
+    'ETAS', 'ETAS_Forecast_File',                                           # schema_name, table_name
+    column_defs=[                                                           # column definitions
+        Column.define('ETAS_Forecast', builtin_types.ermrest_rid)           # a key we will use as fkey column
+    ],
+    fkey_defs=[                                                             # foreign key definitions
+        ForeignKey.define(                                                  # this FKey will reference ETAS_Forecast row
+            ['ETAS_Forecast'],                                              # fkey columns: list of 1+ column(s) from this table
+            'ETAS', 'ETAS_Forecast', ['RID']                                # referenced key: schema name, table name, and list of 1+ key columns
+        )
+    ]
+))
 
-    sys.exit(0)
-    """
-    """
+
+# ETAS Evaluation --------------------------------------------------------------------------------#
+
+# Create the tables at the "Evaluation" level of the table hierarchy
+
+# ETAS_Evaluation
+model.schemas['ETAS'].create_table(Table.define(
+    'ETAS_Evaluation',
+    column_defs=[
+        Column.define('Evaluation_Description', builtin_types.text),        # free-text description
+        Column.define('ETAS_Forecast', builtin_types.ermrest_rid)           # foreign key column
+    ],
+    fkey_defs=[
+        ForeignKey.define(  # this FKey will reference the ETAS_Forecast table
+            ['ETAS_Forecast'],                                              # foreign key column (list allows 1+)
+            'ETAS', 'ETAS_Forecast', ['RID']                                # referenced schema, table, columns
+        )
+    ]
+))
+
+
+# ETAS_Evaluation_File
+#  This is to store the readme.md, meta.json, config.json, evaluation_catalog.json files (one file reference per row)
+model.schemas['ETAS'].create_table(Table.define_asset(
+    'ETAS', 'ETAS_Evaluation_File',
+    column_defs=[
+        Column.define('ETAS_Evaluation', builtin_types.ermrest_rid)         # foreign key column
+    ],
+    fkey_defs=[
+        ForeignKey.define(  # This FKey will reference the ETAS_Evaluation table
+            ['ETAS_Evaluation'],
+            'ETAS', 'ETAS_Evaluation', ['RID']
+        )
+    ]
+))
+
+
+# ETAS_Evaluation_Result
+#  This is for storing the result files (one file reference per row)
+model.schemas['ETAS'].create_table(Table.define_asset(
+    'ETAS', 'ETAS_Evaluation_Result',
+    column_defs=[
+        Column.define('ETAS_Evaluation', builtin_types.ermrest_rid),        # foreign key columns
+        Column.define('Evaluation_Type', builtin_types.text)                # type of the evaluation
+    ],
+    fkey_defs=[
+        ForeignKey.define(  # This FKey will reference the ETAS_Evaluation table
+            ['ETAS_Evaluation'],
+            'ETAS', 'ETAS_Evaluation', ['RID']
+        )
+    ]
+))
+
+
+# ETAS_Evaluation_Plot
+#  This is for storing the plot files (one file reference per row)
+model.schemas['ETAS'].create_table(Table.define_asset(
+    'ETAS', 'ETAS_Evaluation_Plot',
+    column_defs=[
+        Column.define('ETAS_Evaluation_Result', builtin_types.ermrest_rid), # foreign key column
+    ],
+    fkey_defs=[
+        ForeignKey.define(  # This FKey will reference the ETAS_Evaluation_Result table
+            ['ETAS_Evaluation_Result'],
+            'ETAS', 'ETAS_Evaluation_Result', ['RID']
+        )
+    ]
+))
+
+print('Congratulations! You have created a DERIVA catalog model.')
+print('https://{hostname}/chaise/recordset/#{catalog_id}/ETAS:ETAS_Forecast'.format(hostname=hostname, catalog_id=catalog_id))
+"""
+"""
